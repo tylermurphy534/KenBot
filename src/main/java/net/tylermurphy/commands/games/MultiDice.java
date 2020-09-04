43eec6120658b8d5c7723d2eb82ec9d214d6c9bb
Tyler Murphy <tylermurphy534@gmail.com>
2020-09-03 21:38:19 -0400

    changed multi dice command

--- src/main/java/net/tylermurphy/commands/games/MultiDice.java
+++ src/main/java/net/tylermurphy/commands/games/MultiDice.java
@@ -38,26 +38,43 @@ public class MultiDice implements ICommand {
 			channel.sendMessage(":x: Dice sides must be between 4 and 100").queue();
 			return;
 		}
-		if(dice < 2 || dice > 10) {
-			channel.sendMessage(":x: Dice must be between 2 and 10").queue();
+		if(dice < 2 || dice > 100) {
+			channel.sendMessage(":x: Dice must be between 2 and 100").queue();
 			return;
 		}
-		EmbedBuilder builder = EmbedUtils.getDefaultEmbed()
-				.setColor(Color.yellow)
-				.setTitle("Dice Roll");
-		int total = 0;
-		for(int i=0;i<dice;i++) {
-			int rand = (int)(Math.random()*sides)+1;
-			total+= rand;
-			builder.appendDescription(String.format(
-							"`Dice %s:` rolled %s\n",
-							i+1,
-							rand,
-							sides
-					));
+		try {
+			int total = 0;
+			EmbedBuilder builder = EmbedUtils.getDefaultEmbed()
+					.setColor(Color.yellow)
+					.setTitle("Dice Roll");
+			for(int i=0;i<dice;i++) {
+				int rand = (int)(Math.random()*sides)+1;
+				total+= rand;
+				builder.appendDescription(String.format(
+								"`Dice %s:` rolled %s\n",
+								i+1,
+								rand,
+								sides
+						));
+			}
+			builder.appendDescription("\n`Total:` "+total);
+			channel.sendMessage(builder.build()).queue();
+		}catch(Exception e) {
+			int total = 0;
+			EmbedBuilder builder = EmbedUtils.getDefaultEmbed()
+					.setColor(Color.yellow)
+					.setTitle("Dice Roll");
+			String text = "```";
+			for(int i=0;i<dice;i++) {
+				int rand = (int)(Math.random()*sides)+1;
+				total+= rand;
+				text += rand;
+				if(i+1<dice) text += ",";
+			}
+			text += ("```\n`Total:` "+total);
+			builder.setDescription(text);
+			channel.sendMessage(builder.build()).queue();
 		}
-		builder.appendDescription("\n`Total:` "+total);
-		channel.sendMessage(builder.build()).queue();
 	}
 
 	public String getInvoke() {
